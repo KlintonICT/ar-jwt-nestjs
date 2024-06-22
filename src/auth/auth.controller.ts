34c59fcbb32bf86444541f272149ade2f75e76d8
KlintonICT <chhunklinton91@gmail.com>
2024-06-22 17:48:16 +0700

    feat: created logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
@@ -8,22 +18,29 @@ import { TTokens } from './types';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('/local/signup')
+  @Post('local/signup')
+  @HttpCode(HttpStatus.CREATED)
   signupLocal(@Body() dto: AuthDto): Promise<TTokens> {
     return this.authService.signupLocal(dto);
   }
 
-  @Post('/local/sign-in')
+  @Post('local/sign-in')
+  @HttpCode(HttpStatus.OK)
   signInLocal(@Body() dto: AuthDto): Promise<TTokens> {
     return this.authService.signInLocal(dto);
   }
 
-  @Post('/logout')
-  logout() {
-    return this.authService.logout();
+  @UseGuards(AuthGuard('jwt'))
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  logout(@Req() req: Request) {
+    const user = req.user;
+
+    return this.authService.logout(user['id']);
   }
 
-  @Post('/refresh')
+  @UseGuards(AuthGuard('jwt-refresh'))
+  @Post('refresh')
   refreshTokens() {
     return this.authService.refreshTokens();
   }
